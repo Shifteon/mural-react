e9bb8e0e080691c6be9b9ab87c9f9881f5011c11
Shifteon <bpwyatt04@gmail.com>
2022-11-26 10:41:53 -0700

    Full artwork can now be viewed

--- src/components/artwork.js
+++ src/components/artwork.js
@@ -3,17 +3,34 @@ import axios from "axios";
 import { imgSrcUrl } from "../constants";
 import { Link } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import { useOutsideClick } from "../hooks/outsideClick";
 
 export const Artwork = ({ artwork }) => {
   const [cookies] = useCookies("currentUser");
 
   const [url, setUrl] = React.useState('');
+  const [style, setStyle] = React.useState({});
   const [isShown, setIsShown] = React.useState(false);
   const [isTextShown, setIsTextShown] = React.useState(false);
+  const [showFullImage, setShowFullImage] = React.useState(false);
 
   React.useEffect(() => {
     getUrl();
   }, []);
+
+  const wrapperRef = React.useRef(null);
+  useOutsideClick(wrapperRef, () => {
+    setShowFullImage(false);
+    setStyle({});
+  });
+
+  const handleImageClick = () => {
+    setShowFullImage(true);
+    const position = window.pageYOffset;
+    setStyle({
+      top: `${position + 10}px`
+    });
+  }
 
   React.useEffect(() => {
     setTimeout(() => {
@@ -32,13 +49,16 @@ export const Artwork = ({ artwork }) => {
   };
 
   return (
-    <div className="single-artwork"
+    <div className={showFullImage ? "full-image" : "single-artwork"}
       onMouseEnter={() => setIsShown(true)}
       onMouseLeave={() => { setIsShown(false); setIsTextShown(false) }}
+      ref={wrapperRef}
+      style={style}
     >
-      <img src={url} loading='lazy' />
-      <div    className={`${isShown ? 'display' : ''}`} >
+      <img src={url} loading='lazy' onClick={handleImageClick} />
+      <div    className={`${isShown ? 'display text' : 'text'}`} >
         <h1   className={`${isTextShown ? 'display' : ''}`} >{artwork.name}</h1>
+        <p>{artwork.description}</p>
         <Link className={`${isTextShown ? 'display' : ''}`} to="/profile" state={{
           isCurrentUser: artwork.username == cookies.currentUser.username,
           username: artwork.username
--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -2,6 +2,8 @@ import React from "react";
 import axios from "axios";
 import { baseRequestUrl } from "../constants";
 import { useNavigate } from "react-router-dom";
+import { Store } from 'react-notifications-component';
+import { notificationOptions } from "../constants";
 
 const SignUp = () => {
   const requestUrl = `${baseRequestUrl}auth/signup`;
@@ -57,10 +59,20 @@ const SignUp = () => {
     })
       .then(response => {
         console.log(response);
+        Store.addNotification({
+          ...notificationOptions,
+          title: "Succesfully Created Account",
+          type: "success"
+        });
         navigate('/login');
       })
       .catch(error => {
         console.log(error);
+        Store.addNotification({
+          ...notificationOptions,
+          title: "Error Creating Account",
+          type: "danger"
+        });
       })
   };
 
@@ -71,20 +83,20 @@ const SignUp = () => {
         <img src="/images/mural-logo-stacked.png" />
         <h2>Account Info</h2>
         <div>
-          <label for="email">
+          <label htmlFor="email">
             Email
           </label>
-          <input type="text" name="email" value={email} onChange={handleEmailChange} />
+          <input required type="text" name="email" value={email} onChange={handleEmailChange} />
           <label>
             Username
             
           </label>
-          <input type="text" value={username} onChange={handleUsernameChange} />
+          <input required type="text" value={username} onChange={handleUsernameChange} />
           <label>
             Password
             
           </label>
-          <input type="password" value={password}  onChange={handlePasswordChange} />
+          <input required type="password" value={password}  onChange={handlePasswordChange} />
         </div>
         <h2>Profile Info</h2>
         <div>
@@ -92,17 +104,17 @@ const SignUp = () => {
             Name
             
           </label>
-          <input type="text" value={name}  onChange={handleNameChange} />
+          <input required type="text" value={name}  onChange={handleNameChange} />
           <label>
             Bio
             
           </label>
-          <input type="text" value={bio}  onChange={handleBioChange} />
+          <input required type="text" value={bio}  onChange={handleBioChange} />
           <label>
             Profile Picture
           
           </label>
-          <input type="file" name="profilePic" onChange={handleProfilePicChange} />
+          <input required type="file" name="profilePic" onChange={handleProfilePicChange} />
         </div>
         {/* <input type="submit" value="Submit" /> */}
         <button type="submit" >Signup</button>
